442acaadd1458912078d2324440ff55d89e9bc18
Sal0-UA <maxymholub@gmail.com>
2023-03-24 20:51:52 +0200

    Firs commit with basic functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+def force1(mass1, mass2, radius, G=6.67*10^-11):
+    force = G * ((mass1 * mass2) + (radius^2))
+
+    return force
+
+def force2(mass,g=9.8):
+    force = g * mass
+
+    return force
